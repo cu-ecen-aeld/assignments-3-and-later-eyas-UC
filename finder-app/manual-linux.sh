34c873ca0e2eadd1c78fb8a01949d12e9ce833a0
Eyas <eyas077@gmail.com>
2025-09-07 18:47:49 +0100

    adding-correct-manual-script

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -4,6 +4,10 @@
 
 set -e
 set -u
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' 
 REPO_DIR=${PWD}
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
@@ -39,14 +43,13 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    echo "4"
-    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    # make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules # skipped as its done by default for ours specific arch
     make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 
 fi
 
 echo "Adding the Image in outdir"
-
+cp -r ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -71,14 +74,14 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+make -j$(nproc) distclean
+make -j$(nproc)  defconfig
+make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-make -j$(nproc) distclean
-make -j$(nproc)  defconfig
-make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make -j$(nproc)  CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 
@@ -88,34 +91,49 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-cp ${REPO_DIR}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
-cp ${REPO_DIR}/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
-cp ${REPO_DIR}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
-cp ${REPO_DIR}/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+# ---------------------------------------------------------------------------
+# -------------------------Start of Assisted work ---------------------------
+# ---------------------------------------------------------------------------
+# got the sysroot correct copy from this repo
+# https://github.com/cu-ecen-aeld/assignments-3-and-later-thanhtungmanutd/tree/main/finder-app
+# to help fix the can't run /bin/sh issue when running QEMU
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+# ---------------------------------------------------------------------------
+# -------------------------END of Assisted work -----------------------------
+# ---------------------------------------------------------------------------
+
 # TODO: Make device nodes
-sudo mknod  -m 666 dev/null c 1 3
-sudo mknod  -m 600 dev/console c 5 1
+sudo mknod  -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod  -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
 
 # TODO: Clean and build the writer utility
-cd "${FINDER_APP_DIR}"; make clean ; make
+# using cross_compile to make sure writter works on arm64 linux
+cd "${FINDER_APP_DIR}"; make clean ; make CROSS_COMPILE=${CROSS_COMPILE}
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-cp "${FINDER_APP_DIR}"/start-qemu-app.sh    ${OUTDIR}/rootfs/home
-cp "${FINDER_APP_DIR}"/start-qemu-terminal.sh    ${OUTDIR}/rootfs/home
-cp "${FINDER_APP_DIR}"/autorun-qemu.sh   ${OUTDIR}/rootfs/home
-cp "${FINDER_APP_DIR}"/writer    ${OUTDIR}/rootfs/home
-cp "${FINDER_APP_DIR}"/writer.sh    ${OUTDIR}/rootfs/home
-cp "${FINDER_APP_DIR}"/finder.sh   ${OUTDIR}/rootfs/home
-cp "${FINDER_APP_DIR}"/dependencies.sh   ${OUTDIR}/rootfs/home
+sudo cp -r  ${FINDER_APP_DIR}/* ${OUTDIR}/rootfs/home
+cd ..
+cp -r conf ${OUTDIR}/rootfs/
+
+echo -e "${GREEN}DONE copying files${NC}"
+
 
 # TODO: Chown the root directory
 cd ${OUTDIR}/rootfs/
 sudo chown -R root:root *
+sudo chmod +x ${OUTDIR}/rootfs/bin
 
 # TODO: Create initramfs.cpio.gz
-# -H format
-# -v verbose
-# -o use  ARCHIVE-NAME instead of start output
+# -H format -v verbose -o use  ARCHIVE-NAME instead of start output
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+echo -e "${GREEN}DONE with creating cpio file${NC}"
+gzip -f  ${OUTDIR}/initramfs.cpio
+echo -e "${GREEN}DONE with creating gzipping initramfs.cpio.gz${NC}"
+
 
