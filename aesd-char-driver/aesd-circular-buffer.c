e2a9e7a1b6ed4ef2d1bcf760bdf534c7801e25d8
Eyas Elhashmi <eyas077@gmail.com>
2026-04-14 00:04:03 +0100

    assignment-7

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -8,6 +8,8 @@
  *
  */
 
+#include <stddef.h>
+#include <stdio.h>
 #ifdef __KERNEL__
 #include <linux/string.h>
 #else
@@ -32,6 +34,35 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
+    // easy case
+    size_t accumulation = 0;
+
+    // buffer->in_offs can never be greater than buffer->out_offs
+    // the start is always from buffer->offs to buffer->in_offs
+    // iff in ==out then buffer then check the full to loop through everything otherwise return null;
+    if (buffer->full == true && (buffer->in_offs == buffer->out_offs))
+    {
+    }
+    for (size_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++)
+    {
+        size_t current_element = (i +  buffer->in_offs) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+        // printf("char_offset=%zu out=%i in=%i i=%zu, current_element=%zu, accumulation=%zu, str=%s\n",char_offset, buffer->out_offs,buffer->in_offs, i, current_element,accumulation,buffer->entry[current_element].buffptr);
+        if ((buffer->in_offs< buffer->out_offs) && (buffer->in_offs>current_element))
+        {
+            // printf("returning null here \n");
+            return NULL;
+        }
+        size_t current_size = buffer->entry[current_element].size;
+        if ((accumulation  + current_size) > char_offset)
+        {
+            // printf("returning current element here %li\n",current_element);
+            * entry_offset_byte_rtn = char_offset - accumulation; 
+            return &buffer->entry[current_element];
+        }
+        accumulation += current_size;
+
+
+    }
     return NULL;
 }
 
@@ -47,6 +78,21 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+    // case where buffer is full, so we need to move the out_offs to the next entry to be overwritten
+    if (buffer->full)
+    {
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; 
+    }
+    // add the new entry to the buffer
+    buffer->entry[buffer->in_offs] = *add_entry;
+    // move the in_offs to the next location
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; 
+    // if the buffer is now full, set the full flag to true 
+    // the buffer has rotated and caught up to the out_offs
+    if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }
 }
 
 /**
